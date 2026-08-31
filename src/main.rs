08217a75f884134d2c3313f7d75d0d36a88899f3
BlueOceanVale <bluecloudyriver@gmail.com>
2026-08-31 22:00:18 +0530

    feat: add file info endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,47 @@
-use axum::{Router, routing:: get};
-use axum::Json;
+use axum::extract::Query;
+use axum::{Json, Router};
+use axum::routing::get;
+use serde::de::value;
 use tokio::net::TcpListener;
+use serde::{Deserialize, Serialize};
+use std::path::Path
 
+#[derive(Serialize)]
+struct FileInfo {
+    name: String,
+    extension: String,
+}
+
+#[derive(Deserialize)]
+struct FileQuery {
+    name: String,
+}
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let app = Router::new()
         .route("/health", get(health))
-        .unwrap();
+        .route("/files/info", get(file_info));
 
-    
     let listener = TcpListener::bind("127.0.0.1:3000").await?;
-
     axum::serve(listener, app).await?;
+
+    Ok(())
 }
 
-fn health() -> Json<String> {
-    Json("ok".to_string())
+async fn health() -> Json<&'static str> {
+    Json("ok")
+}
+
+async fn file_info(
+    Query(quer): Query<FileQuery>,
+) -> Json<FileInfo>{
+    let path = Path::new(&quer.name);
+    let extension = match path.extension() {
+        Some(value) => value.to_string_lossy().to_string(),
+        None => String::from("none"),
+    };
+
+
+    Json(FileInfo { name: quer.name, extension })
 }
 
