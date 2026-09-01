ef60037d3dabe350bcabccb385ef47a2fe128b06
BlueOceanVale <bluecloudyriver@gmail.com>
2026-09-01 15:46:59 +0530

    feat: add image conversion feature(tested)

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,23 @@ struct FileQuery {
     name: String,
 }
 
+#[derive(Deserialize)]
+struct ConvertRequest {
+    input: String,
+    format: String,
+}
+
+#[derive(Serialize)]
+struct ConvertResponse {
+    output: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let app = Router::new()
         .route("/health", get(health))
-        .route("/files/info", post(file_info));
+        .route("/files/info", post(file_info))
+        .route("/images/convert", post(convert_image_handler));
 
     let listener = TcpListener::bind("127.0.0.1:3000").await?;
     axum::serve(listener, app).await?;
@@ -40,7 +52,26 @@ async fn file_info(
         None => String::from("none"),
     };
 
-
     Json(FileInfo { name: quer.name, extension })
+}
+
+async fn convert_image_handler(
+    Json(req): Json<ConvertRequest>,
+) -> Json<ConvertResponse> {
+    let img = match image::open(req.input) {
+        Ok(value) => value,
+        Err(_) => return Json(ConvertResponse { output: "failed to convert image".to_string() })
+    };
+    let format = match req.format.as_str() {
+        "webp" => image::ImageFormat::WebP,
+        "png" => image::ImageFormat::Png,
+        "jpeg" => image::ImageFormat::Jpeg,
+        _ => return Json(ConvertResponse { output: "unsupported format".to_string() })
+    };
+    let form = format!("output.{}", req.format);
+    match img.save_with_format(form, format) {
+        Ok(_) => Json(ConvertResponse { output: "image saved".to_string() }),
+        Err(_) => Json(ConvertResponse { output: "failed to save image".to_string() }),
+    }
 }
 
