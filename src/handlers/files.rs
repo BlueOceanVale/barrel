1a8f3624f096c2f45d385d3362a49619207feb2c
BlueOceanVale <bluecloudyriver@gmail.com>
2026-09-16 19:51:38 +0530

    feat: add file format endpoint

--- src/handlers/files.rs
+++ src/handlers/files.rs
@@ -1,4 +1,4 @@
-use axum::Json;
+use axum::{Json, extract::multipart::Multipart, response::Response};
 use crate::models::models::{FileInfo, FileQuery};
 use std::path::Path;
 
@@ -13,4 +13,22 @@ pub async fn file_info(
     };
 
     Json(FileInfo { name: quer.name, extension })
+}
+
+pub async fn file_format(
+    mut multipart: Multipart,
+) -> Response {
+    loop {
+        let Some(file) = match multipart.next_field() {
+            Ok(file) => file,
+            Ok(None) => break,
+            Err(_) => {
+                return Response::builder()
+                    .header("Content-Type", "text/plain")
+                    .body("multipart error".into())
+                    .unwrap()
+            }
+        }
+    }
+
 }
